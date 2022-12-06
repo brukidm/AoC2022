e66c8b19ce9bcf465b576356b6641818f780c1ef
Mislav <mislavbrusac0@gmail.com>
2022-12-06 10:24:58 +0100

    6

---
+++ Day06/2.py
@@ -0,0 +1,14 @@
+with open(r"input") as f:
+    values = f.read().split('\n')
+    values = list(values[0])
+    last_four = []
+    i = 0
+    for value in values:
+        if len(last_four) == 14 and len(last_four) == len(set(last_four)):
+            print(i)
+            break
+        if len(last_four) == 14:
+            last_four.pop(0)
+        last_four.append(value)
+        i += 1
+        
